8888171402899f3c69b0443e0081f75f9bc258ad
Nuchsongsin Jirakit <jirakit.4609@gmail.com>
2025-03-25 19:26:43 +0700

    third

--- src/modules/videos/ui/views/video-view.tsx
+++ src/modules/videos/ui/views/video-view.tsx
@@ -1,3 +1,6 @@
+import { VideoSection } from "../sections/video-section";
+
+
 interface VideoViewProps {
   videoId: string;
 }
@@ -5,7 +8,11 @@ interface VideoViewProps {
 export const VideoView = ({ videoId }: VideoViewProps) => {
   return (
     <div className="flex flex-col max-w-[1700px] mx-auto pt-2.5 px-4 mb-10">
-      <p>Video!</p>
+      <div className="flex flex-col xl:flex-row gap-6">
+        <div className="clex-1 min-w-0">
+          <VideoSection videoId={videoId} />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
