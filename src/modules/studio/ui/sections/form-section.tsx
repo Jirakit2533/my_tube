66984112ab75f43cf840af5a15947de8d00daaed
Nuchsongsin Jirakit <jirakit.4609@gmail.com>
2025-03-27 15:28:49 +0700

    Fifth

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -281,7 +281,7 @@ const FormSectionSuspence = ({ videoId }: FromSectionProps) => {
               
             </div>
             <div className="flex flex-col gap-y-8 lg:col-span-2">
-              <div className="flex flex-col gap-4 bg[@f9f9f9] rounded-xl overflow-hidden h-fit">
+              <div className="flex flex-col gap-4 bg-[#F9F9F9] rounded-xl overflow-hidden h-fit">
                 <div className="aspect-video overflow-hidden relative">
                   <VideoPlayer 
                     playbackId={video.muxPlaybackId}
@@ -296,7 +296,7 @@ const FormSectionSuspence = ({ videoId }: FromSectionProps) => {
                       </p>
                       <div className="flex items-center gap-x-2">
                         <Link href={`/videos/${video.id}`}>
-                          <p className="line-clam-1 text-sm text-blue-500">
+                          <p className="line-clamp-1 text-sm text-blue-500">
                             {fullUrl}
                           </p>
                         </Link>
